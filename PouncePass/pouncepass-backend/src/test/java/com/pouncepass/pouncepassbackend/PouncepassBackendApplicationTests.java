3b778490091704cb452ddfcbe58a7fbd8be74ebf
Dobson Dunavant <dobsonddev@gmail.com>
2023-09-29 00:37:04 -0400

    Initialized React frontend and SpringBoot backend

---
+++ PouncePass/pouncepass-backend/src/test/java/com/pouncepass/pouncepassbackend/PouncepassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pouncepass.pouncepassbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PouncepassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
