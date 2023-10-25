94f97ffdebe928e0e960eca4250fa9fe9658a3b6
Dobson Dunavant <69472321+dobsonddev@users.noreply.github.com>
2023-10-25 12:00:47 -0400

    D's Event.java

--- TP D4/Event.java
+++ TP D4/Event.java
@@ -1,40 +1,139 @@
-import java.util.Date;
-import java.util.ArrayList;
-
 public class Event {
     private int eventId;
-    private String eventTitle;
-    private Venue location;
-    private Date date;
-    private int time;
-    private ArrayList<Ticket> tickets;
+    private String name;
+    private Venue venue;
+    private String dateTime;
+    private float ticketPrice;
+    private int totalTickets;
+    private int availableTickets;
 
-    public Event() {
-
+    // Getters
+    public int getEventId() {
+        return eventId;
     }
-    public void displayEvent() {
 
+    public String getName() {
+        return name;
     }
-    public void holdTicket(Ticket ticket) {
 
+    public Venue getVenue() {
+        return venue;
     }
-    public void updateTickets(Ticket ticket) {
 
+    public String getDateTime() {
+        return dateTime;
     }
-    //getters&setters
-    public int getEventId() { return this.eventId; }
-    public String getEventTitle() { return this.eventTitle; }
-    public Venue getLocation() { return this.location; }
-    public Date getDate() { return this.date; }
-    public int getTime() { return this.time; }
 
-    public void updateLocation(Venue input) {
-
+    public float getTicketPrice() {
+        return ticketPrice;
     }
-    public void updateDate(Date input) {
 
+    public int getTotalTickets() {
+        return totalTickets;
     }
-    public void updateTime(int input) {
 
+    public int getAvailableTickets() {
+        return availableTickets;
+    }
+
+    // Setters
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setTicketPrice(float ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+
+    public void setAvailableTickets(int availableTickets) {
+        this.availableTickets = availableTickets;
+    }
+
+    // Methods
+    public boolean createEvent() {
+        // Implementation to create an event
+        return true;
+    }
+
+    public boolean updateEvent() {
+        // Implementation to update an event
+        return true;
+    }
+
+    public boolean deleteEvent() {
+        // Implementation to delete an event
+        return true;
+    }
+
+    public boolean bookTicket() {
+        if (availableTickets > 0) {
+            availableTickets--;
+            return true;
+        }
+        return false;
+    }
+
+    public void cancelTicket() {
+        availableTickets++;
     }
 }
+
+
+
+//Below is Leo's Event class
+// import java.util.Date;
+// import java.util.ArrayList;
+
+// public class Event {
+//     private int eventId;
+//     private String eventTitle;
+//     private Venue location;
+//     private Date date;
+//     private int time;
+//     private ArrayList<Ticket> tickets;
+
+//     public Event() {
+
+//     }
+//     public void displayEvent() {
+
+//     }
+//     public void holdTicket(Ticket ticket) {
+
+//     }
+//     public void updateTickets(Ticket ticket) {
+
+//     }
+//     //getters&setters
+//     public int getEventId() { return this.eventId; }
+//     public String getEventTitle() { return this.eventTitle; }
+//     public Venue getLocation() { return this.location; }
+//     public Date getDate() { return this.date; }
+//     public int getTime() { return this.time; }
+
+//     public void updateLocation(Venue input) {
+
+//     }
+//     public void updateDate(Date input) {
+
+//     }
+//     public void updateTime(int input) {
+
+//     }
+// }
